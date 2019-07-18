5f8ba89ace4dfdf1252a54fa37b754c6cc54114a
quarcko <31532153+quarcko@users.noreply.github.com>
2019-07-18 23:21:04 +0300

    RGBA handling enchancements

--- light.py
+++ light.py
@@ -41,7 +41,7 @@ class XiaomiGatewayLight(XiaomiGwDevice, Light):
     @property
     def supported_features(self):
         return SUPPORT_BRIGHTNESS | SUPPORT_COLOR
-
+    
     def turn_on(self, **kwargs):
         if ATTR_HS_COLOR in kwargs:
             self._hs = kwargs[ATTR_HS_COLOR]
@@ -51,7 +51,12 @@ class XiaomiGatewayLight(XiaomiGwDevice, Light):
         argb = (self._brightness,) + rgb
         argbhex = binascii.hexlify(struct.pack("BBBB", *argb)).decode("ASCII")
         argbhex = int(argbhex, 16)
-        self._send_to_hub({ "method": "set_rgb", "params": [argbhex] })
+        
+        if argbhex <= 16777215:
+            self._send_to_hub({ "method": "toggle_light", "params": ["on"] })
+        else:
+            self._send_to_hub({ "method": "set_rgb", "params": [argbhex] })
+        
         self._state = True
         self.schedule_update_ha_state()
 
@@ -63,11 +68,23 @@ class XiaomiGatewayLight(XiaomiGwDevice, Light):
     def parse_incoming_data(self, model, sid, event, params):
 
         light = params.get("light")
-        if light is not None:
-            if light == 'on':
-                self._state = True
-            elif light == 'off':
-                self._state = False
-            return True
+        rgba_raw = params.get("rgb")
+        
+        if None not in (rgba_raw, light):
+            rgbhexstr = "%x" % rgba_raw
+            if len(rgbhexstr) <= 8:
+                rgbhexstr = rgbhexstr.zfill(8)
+                rgbhex = bytes.fromhex(rgbhexstr)
+                rgba = struct.unpack('BBBB', rgbhex)
+                brightness = rgba[0]
+                rgb = rgba[1:]
+        
+                if light == 'on' or rgba_raw > 0:
+                    self._state = True
+                    self._brightness = brightness
+                    self._hs = color_util.color_RGB_to_hs(*rgb)
+                else:
+                    self._state = False
+                return True
 
         return False
