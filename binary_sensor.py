6e439f490785caf628d521155a0878e5dd28e109
Seweryn Zeman <seweryn.zeman@jazzy.pro>
2019-09-17 09:57:20 +0200

    Fixed GW timeout listeners; Shorter ping times; Added auto-destate

--- binary_sensor.py
+++ binary_sensor.py
@@ -1,7 +1,11 @@
 import logging
+from datetime import timedelta
 
 from homeassistant.components.binary_sensor import (
     BinarySensorDevice, DEVICE_CLASSES)
+
+from homeassistant.helpers.event import async_track_point_in_utc_time
+import homeassistant.util.dt as dt_util
 
 from . import DOMAIN, CONF_DATA_DOMAIN, CONF_SENSOR_SID, CONF_SENSOR_CLASS, CONF_SENSOR_NAME, EVENT_VALUES, XiaomiGwDevice
 
@@ -60,11 +64,10 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
         if sid is None or device_class is None:
             continue
 
-        _LOGGER.info("Registering " + str(device_class) + " sid " + str(sid))
-
         gateway.known_sids.append(sid)
 
         if device_class in all_device_classes:
+            _LOGGER.info("Registering " + str(device_class) + " sid " + str(sid) + " as binary_sensor")
             entities.append(XiaomiGwBinarySensor(gateway, device_class, sid, name))
 
     if not entities:
@@ -79,13 +82,15 @@ class XiaomiGwBinarySensor(XiaomiGwDevice, BinarySensorDevice):
     def __init__(self, gw, device_class, sid, name):
         XiaomiGwDevice.__init__(self, gw, "binary_sensor", device_class, sid, name)
 
-        self._last_action = None
-
         # Custom Button device class
         if device_class == DEVICE_CLASS_BUTTON:
             self._device_class = None
         else:
             self._device_class = device_class
+
+        self._last_action = None
+
+        self._state_timer = None
 
     @property
     def is_on(self):
@@ -118,6 +123,22 @@ class XiaomiGwBinarySensor(XiaomiGwDevice, BinarySensorDevice):
                 'entity_id': self.entity_id,
                 'event_type': event_type
             })
+            self._state = event_type
             self._last_action = event_type
+            self._start_state_timer()
 
         return True
+
+    def _start_state_timer(self):
+        """Start state timer."""
+        if self._state_timer is not None:
+            self._state_timer()
+            self._state_timer = None
+        restore_state_time = dt_util.utcnow() + timedelta(seconds=15)
+        self._state_timer = async_track_point_in_utc_time(self.hass, self._stop_state_timer, restore_state_time)
+
+    def _stop_state_timer(self, time):
+        """Stop state timer."""
+        self._state_timer = None
+        self._state = False
+        self.schedule_update_ha_state()
